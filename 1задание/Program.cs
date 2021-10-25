389d434f561f30c8955d8d195980ce8f3da2555f
octop00 <roarr0011@gmail.com>
2021-10-25 22:00:30 +1000

    upd

---
+++ 1задание/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c;
+            Random rnd = new Random();
+            int d = rnd.Next(0,100);
+            Console.WriteLine("Угадайте заданное число");
+
+            do
+            {
+                int.TryParse(Console.ReadLine(), out int a);
+                if (a > d)
+                    Console.WriteLine("Много");
+                if (a < d)
+                    Console.WriteLine("Мало");
+                c = a;
+            }
+            while (c != d);
+            Console.WriteLine("Верно!");
+        }
+    }
+}
